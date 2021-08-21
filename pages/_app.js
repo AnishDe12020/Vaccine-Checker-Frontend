ce0f7a2daebd02832eb6b46ed29d073383db42ef
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-08-21 20:37:42 +0530

    Added dark mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import { ChakraProvider } from "@chakra-ui/react"
 import "../styles/globals.css"
+import theme from "@/styles/theme"
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <Component {...pageProps} theme={theme} />
     </ChakraProvider>
   )
 }
